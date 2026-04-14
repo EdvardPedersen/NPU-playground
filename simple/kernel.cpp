ca240da8739c394b7535d3b741f63d0c617c2a79
Edvard Pedersen <edvard.pedersen@uit.no>
2026-04-14 15:44:41 +0200

    all vectorized, doesn't work

--- simple/kernel.cpp
+++ simple/kernel.cpp
@@ -33,27 +33,52 @@ void passThroughLine(int32_t *in, int32_t *out, int32_t lineWidth, int32_t node,
         aie::vector<bfloat16, VEC_WIDTH> yesyes;
         yesyes = aie::to_vector<bfloat16>(real_yes);
 
-        aie::vector<bfloat16, VEC_WIDTH> y0s = aie::mul(real_yes, 2.24);
-        aie::vector<bfloat16, VEC_WIDTH> x0s = aie::mul(real_xes, 2.47);
+        auto y0s = aie::mul(yesyes, (bfloat16)2.24);
+        y0s = aie::mul(aie::to_vector<bfloat16>(y0s), (bfloat16)stage);
+        bfloat16 temp = 1.12*stage;
+        y0s = aie::sub(y0s, temp);
 
-        //float y0 = y * 2.24 * stage - 1.12 * stage;
-        //float x0 = x * 2.47 * stage - 2 * stage;
+        auto x0s = aie::mul(aie::to_vector<bfloat16>(real_xes), (bfloat16)2.47);
+        x0s = aie::mul(aie::to_vector<bfloat16>(x0s), (bfloat16)stage);
+        temp = 2*stage;
+        x0s = aie::sub(x0s, temp);
 
-        aie::vector<uint16, VEC_WIDTH> iters = 0;
+        aie::vector<uint16, VEC_WIDTH> iters = aie::zeros<uint16>();
+        aie::vector<bfloat16, VEC_WIDTH> xfs = aie::broadcast<bfloat16>(0);
+        aie::vector<bfloat16, VEC_WIDTH> yfs = aie::broadcast<bfloat16>(0);
 
-        bfloat16 xf = 0;
-        bfloat16 yf = 0;
-        uint8_t iter = 0;
+        for(int i = 0; i < 255; i++) {
+            auto xftemp = aie::mul(xfs, xfs);
+            auto yftemp = aie::mul(yfs, yfs);
+            auto together = aie::add(xftemp, yftemp);
+
+            auto xtemp = aie::sub(xftemp, yftemp);
+            xtemp = aie::add(xtemp, x0s);
+
+            auto yf = aie::mul(xfs, (bfloat16)2);
+            yf = aie::mul(aie::to_vector<bfloat16>(yf), yfs);
+            yf = aie::add(yf, x0s);
+
+            auto mask = aie::le(aie::to_vector<bfloat16>(together), (bfloat16)4.0); 
+            auto next_iter = aie::add(iters, (uint16)1);
+            iters = aie::select(next_iter, iters, mask);
+            
+
+        }
+/*
         while(iter < 255 and xf * xf + yf * yf < 4) {
             bfloat16 xtemp = xf*xf - yf * yf + sx0;
             yf = 2 * xf * yf + sy0;
             xf = xtemp;
             iter += 1;
-        }
+        }*/
         //*outPtr++ = iter << 8 | 0xff;
 
-        aie::store_v(outPtr, mods);
-        outPtr += VEC_WIDTH;
+        for(int i = 0; i < VEC_WIDTH; i++) {
+            *outPtr++ = iters[i] << 8 | 0xff;
+        }
+        //aie::store_v(outPtr, iters);
+        //outPtr += VEC_WIDTH;
     }
 }
 
