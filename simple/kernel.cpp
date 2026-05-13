583e671fd383ff0c111b32e90128cb067f010c57
Edvard Pedersen <edvard.pedersen@uit.no>
2026-05-13 15:31:08 +0200

    better perf

--- simple/kernel.cpp
+++ simple/kernel.cpp
@@ -4,9 +4,9 @@
 
 extern "C" {
 
-#define VEC_WIDTH 16
+#define VEC_WIDTH 32
 #define FLOAT_TYPE bfloat16
-#define UINT_TYPE uint32
+#define UINT_TYPE uint16
 
 void passThroughLine(int32_t * __restrict in, int32_t * __restrict out, int32_t lineWidth, int32_t node, uint64_t split, int32_t nodeWidth, int32_t image_width, int32_t image_height, float stage) {
     int32_t *outPtr = out;
@@ -15,7 +15,7 @@ void passThroughLine(int32_t * __restrict in, int32_t * __restrict out, int32_t 
     for(UINT_TYPE i = 0; i < lineWidth; i += VEC_WIDTH) {
         aie::vector<FLOAT_TYPE, VEC_WIDTH> x0s;
         aie::vector<FLOAT_TYPE, VEC_WIDTH> y0s;
-        for(int x = 0; x < VEC_WIDTH; x++) {
+        for(UINT_TYPE x = 0; x < VEC_WIDTH; x++) {
             uint32 s = (i + start + x);
             x0s[x] = (FLOAT_TYPE)((((s % image_width) / (FLOAT_TYPE)image_width) * (FLOAT_TYPE)2.24) - ((FLOAT_TYPE)2));
             y0s[x] = (FLOAT_TYPE)((((s / image_width) / (FLOAT_TYPE)image_height) * (FLOAT_TYPE)2.47) - ((FLOAT_TYPE)1.12));
@@ -32,17 +32,19 @@ void passThroughLine(int32_t * __restrict in, int32_t * __restrict out, int32_t 
             xf = aie::sub(xsq, ysq);
             yf = aie::add(yf, y0s);
             xf = aie::add(xf, x0s);
-            //iters = aie::select(iters, x, aie::lt(aie::add(xsq, ysq), (bfloat16)4.0));
-            for(int y = 0; y < VEC_WIDTH; y++) {
-                if(xsq[y] + ysq[y] < 4.0) iters[y] = x;
+            auto added = aie::add(xsq, ysq);
+            //auto test = aie::lt(added, (bfloat16)4.0);
+            //iters = aie::select(iters, x, test);
+            for(UINT_TYPE y = 0; y < VEC_WIDTH; y++) {
+                if(added[y] > (bfloat16)4.0)  iters[y] = x;
             }
         }
 
         iters = aie::upshift(iters, 8);
         iters = aie::bit_or((UINT_TYPE)0xff, iters);
-        aie::store_v((uint32 *)outPtr, iters);
+        aie::store_v((uint32 *)outPtr, iters.unpack<uint32>());
         outPtr += VEC_WIDTH;
-}
+    }
 }
 
 } // extern "C"
